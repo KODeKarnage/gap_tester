196e399e32e786fcc6481ab282ea4a51905668ed
KODeKarnage <sublime.kev@gmail.com>
2014-11-28 14:38:13 +1100

    added extreme logging

--- gap_testing.py
+++ gap_testing.py
@@ -8,11 +8,12 @@ import pandas as pd
 # eliminate annoying error message
 pd.options.mode.chained_assignment = None
 
+
 def error_message():
     print '--->:::'
-    print '--->::: Usage:        python gap_testing.py [input file*] [output file*] [minimum^] [gap_adj^]'
+    print '--->::: Usage:        python gap_testing.py [input file*] [output file*] [minimum^] [gap_adj^] -debug^'
     print '--->:::               * required '
-    print '--->:::               ^ optional '
+    print '--->:::               ^ optional (you can only use gap_adj if you also supply a minimum'
     print '--->:::'
     print '--->::: input file:   the filename of the excel file to be tested.'
     print '--->:::               The file must be in the same folder as the script.'
@@ -31,6 +32,9 @@ def error_message():
     print '--->::: gap_adj:      the number of standard deviations consituting an investigatable gap.'
     print '--->:::               default = 1.0'
     print '--->:::'
+    print '--->::: -debug:       add this tag if you want to export a comprehensive debug log to the export folder'
+    print '--->:::               debug log is always named "gap_tester_debug.txt"'
+    print '--->:::'
 
 
 def investigate(dataframe, gap, colhead):
@@ -44,19 +48,30 @@ def investigate(dataframe, gap, colhead):
     
     prev_value = 'start'
 
+    log('   -  length of recieved df:   %s' % len(dataframe))
+
+    action_count = {}
+
     for index, value in dataframe.iteritems():
 
         if  prev_value == 'start': 
+
+            action_count['start'] = action_count.get('start', 0) + 1
 
             prev_value = value
             continue
 
         elif not switch:
 
+            action_count['standard'] = action_count.get('standard', 0) + 1
+
             curr_value = value
             diff = curr_value - prev_value
 
             if abs(diff) > gap * CRITICAL_SD:
+
+                action_count['standard'] = action_count.get('standard', 0) - 1
+                action_count['outlier'] = action_count.get('outlier', 0) + 1
 
                 investigate_list.append(index)
                 switch = True
@@ -66,11 +81,16 @@ def investigate(dataframe, gap, colhead):
         else:
             investigate_list.append(index)
 
+            action_count['outlier'] = action_count.get('outlier', 0) + 1
+
+    log('Counts: start= %s, standard= %s, outlier= %s' % (action_count.get('start', 0), action_count.get('standard', 0), action_count.get('outlier', 0)))
+
     return investigate_list
 
 
 def clean(dataframe):
-    # clean the dataset
+    
+    ''' Cleans the dataset '''
 
     # remove all rows without an ID
     df = dataframe[dataframe['SecId'].notnull()]
@@ -99,6 +119,25 @@ def clean(dataframe):
 
     return df
 
+
+# setup extreme logging !!!!!!!!
+extreme_log = []
+
+if '-debug' in sys.argv:
+    logging = True
+else:
+    logging = False
+
+def log(string):
+
+    global logging
+
+    if logging:
+
+        global extreme_log
+        extreme_log.append(str(string))
+
+
 error = False
 
 # Obtain excel filename
@@ -117,21 +156,36 @@ except:
     sys.exit
 
 try:
-    MIN_FUNDS = sys.argv[3]
+    MIN_FUNDS = float(sys.argv[3])
 except:
     print '--->::: minimum not supplied, using default minimum: 6'
     MIN_FUNDS = 6
-    error=True
+    try:
+        if sys.argv[3] != '-debug':
+            error=True
+    except:
+        pass
 
 try:
-    CRITICAL_SD = sys.argv[4]
+    CRITICAL_SD = float(sys.argv[4])
 except:
     print '--->::: gap_adj not supplied, using default: 1.0'
     CRITICAL_SD = 1.0
-    error=True
+    try:
+        if sys.argv[4] != '-debug':
+            error=True
+    except:
+        pass
 
 if error:
     error_message()
+
+
+
+log('input file:  %s ' % filename)     
+log('output file:   %s' % output)   
+log('MIN_FUNDS:  %s' % MIN_FUNDS)
+log('CRITICAL_SD:  %s' % CRITICAL_SD)
 
 print '--->::: Starting Analysis'
 
@@ -167,8 +221,12 @@ df = df.set_index('SecId')
 # retrieve all the categories in the data
 all_categories = df['Morningstar Category'].unique()
 
+log('all_categories:   %s ' % all_categories)
+
 # cycle over all the return columns
 for column in presumed_headings[4:]:
+
+    log('#' * 120)
 
     # dictionary to hold the ids of the funds to investigate
     new_column_data = {}
@@ -179,31 +237,59 @@ for column in presumed_headings[4:]:
     
     # cycle over all the categories in the dataset
     for category in all_categories:
-        
+
+        log('column:   %s, category:   %s' % (column, category))
+
         # create a dataframe containing only the funds in the category, with non-NA data, 
         # grab only the current return column
         returns = df[df['Morningstar Category'] == category][column].dropna()
            
         # if the dataset is too small, then abandon the analysis for this category over this time period
-        if len(returns) < MIN_FUNDS:
+        count_of_returns = len(returns)
+
+        log('Count of returns:   %s' % count_of_returns)
+
+        if count_of_returns < MIN_FUNDS:
+            log('too few returns, skipping category')
             continue
 
         # calculate the median and the stdev for the returns
         median = returns.median()
         stddev = returns.std()
+
+        log('Median:   %s' % median)
+        log('Stddev:   %s' % stddev)
         
         # create two new dataframes with the funds below and above the median      
         low_slice  = returns[returns < median]
+
+        log('count of low slice:   %s' % len(low_slice))
+
         low_slice.sort(axis=0, inplace=True, ascending=False )
+
+        log('count of low slice after sort:   %s' % len(low_slice))
+        
         high_slice  = returns[returns > median]
-        high_slice.sort(axis=0, inplace=True, ascending=True )        
+
+        log('count of high slice:   %s' % len(high_slice))
+
+        high_slice.sort(axis=0, inplace=True, ascending=True )
+
+        log('count of high slice after sort:   %s' % len(high_slice))
 
         # retrieve lists of the id's of the funds that need to be investigated for this return period
+        log('investigating low slice')
         investigate_low  = investigate(low_slice,  stddev, column)  
+        log('length of investigate_low:   %s' % len(investigate_low))
+
+        log('investigating low slice')
         investigate_high = investigate(high_slice, stddev, column)
+        log('length of investigate_high:   %s' % len(investigate_high))
 
         # add the investigation funds for this category to return period investigation dictionary 
         new_column_data = dict(new_column_data.items() + [(x, 'L') for x in investigate_low] + [(y, 'H') for y in investigate_high])
+
+        log('length of new_column_data:   %s' % len(new_column_data))
       
     # create a new column that highlights with an "L" or an "H" which funds need to be investigated
     df['INV '+column] = [new_column_data.get(x, '') for x in list(df.index.values)]
@@ -219,3 +305,20 @@ print '--->::: Exporting to Excel'
 df.to_excel(output, sheet_name='Gap_Results')
 
 print '--->::: Analysis and Export Complete! :::<---'    
+
+# output the log list to a debug_log file.
+if logging:
+
+    print '--->::: Dumping debug log'
+
+    # debug_series = pd.Series(extreme_log)
+
+    # debug_df = pd.DataFrame({"Debug Log":debug_series})
+
+    # debug_df.to_excel('Debug_log', sheet_name="Debug")
+
+    with open("gap_tester_debug.txt", 'w') as f:
+        lines = '\n'.join(extreme_log)
+        f.writelines(lines)
+
+    print '--->::: Debug log pinched off :::<---'    
